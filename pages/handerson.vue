112a9792625f6c3bebf6383199f138e58aa98b8b
Handerson Marinho <handerson.marinho@nelnet.net>
2023-05-18 14:06:40 -0500

    getting there

---
+++ pages/handerson.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="content">
+    <div class="row">
+      <div class="col-6">
+        FORM HERE
+      </div>
+      <div class="col-6">
+        <h5>Project Estimation</h5>
+        <table>
+          <tr>
+            <td>
+              <span>{{ projectSize }}</span>
+            </td>
+            <td>Size of Project</td>
+          </tr>
+          <tr>
+            <td>
+              <span>{{ numberOfDevelopers }}</span>
+            </td>
+            <td># of Developer</td>
+          </tr>
+          <tr>
+            <td>
+              <span>{{ numberOfQa }}</span>
+            </td>
+            <td># of Quality Analysts</td>
+          </tr>
+          <tr>
+            <td>
+              <span>{{ timeToComplete }}</span>
+            </td>
+            <td>Approximate time to complete</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // middleware: "auth",
+
+  layout: "empty",
+
+  components: {},
+
+  computed: {},
+
+  data() {
+    return {
+      projectSize: 0,
+      numberOfDevelopers: 0,
+      numberOfQa: 0,
+      timeToComplete: 0,
+
+      projectName: "",
+      businessStatment: "",
+      isNewBusiness: false,
+      isNewTech: false,
+      requirements: [
+        {
+          code: 0,
+          description: "",
+          cost: 0,
+          scope: 0,
+          time: 0,
+        },
+      ],
+    };
+  },
+
+  mounted() {
+    this.runCalculation();
+  },
+
+  methods: {
+    runCalculation() {
+      // TODO:
+      // This is our magic
+
+      this.projectSize = 200;
+      this.numberOfDevelopers = 200;
+      this.numberOfQa = 200;
+      this.timeToComplete = 200;
+    },
+  },
+};
+</script>
+<style scoped></style>
